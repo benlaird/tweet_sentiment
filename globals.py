7d7b336b2947ba51b2728971acde8ee55540ae3a
Ben Johnson-Laird <benjohnsonlaird@gmail.com>
2020-05-31 18:06:11 -0400

    Added variables for dir locations & made them depenedent on whether this is a Kaggle notebook submission

--- globals.py
+++ globals.py
@@ -1,8 +1,18 @@
 class Global:
     pipe = None
+    kaggle_submission = False
+    input_dir = None
+    output_dir = None
 
     def __init__(self):
         Global.pipe = None
+        Global.kaggle_submission = False
+        if Global.kaggle_submission:
+            Global.input_dir = "/kaggle/input/tweet-sentiment-extraction/"
+            Global.output_dir = "/kaggle/working/"
+        else:
+            Global.input_dir = "./data/tweet-sentiment-extraction/"
+            Global.output_dir = "./"
 
     @classmethod
     def set_pipe(self, p):
